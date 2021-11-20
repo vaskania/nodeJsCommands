5ccb7b0c01232d475aac2c496bb0f526b8817f5c
vaskaHome <vaskania26@gmail.com>
2021-11-20 13:35:27 +0400

    testing lint-staged

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ const server = http.createServer((req, res) => {
 });
 
 // eslint-disable-next-line no-console
-server.listen(3000, () => console.log('app is running'));
+server.listen(3000, () => console.log('App is running'));
